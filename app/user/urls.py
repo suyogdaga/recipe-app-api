274dc5f70c883f01010da8bcc65db20440101b78
Suyog Daga <sdaga@quinstreet.com>
2024-03-29 18:12:23 -0400

    Added user api

---
+++ app/user/urls.py
@@ -0,0 +1,12 @@
+"""
+url mapping for user api
+"""
+
+from django.urls import path
+from user import views
+
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
