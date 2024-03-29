76422f5d6f7c8b91d6d04c8fbff039b175c5763d
Suyog Daga <sdaga@quinstreet.com>
2024-03-29 16:30:00 -0400

    Custom admin page

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin page for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """ Test the admin site. """
+
+    def setUp(self):
+        """ Setup the tests. """
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@exmaple.com',
+            password = 'password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user=get_user_model().objects.create_user(
+            email = 'user@example.com',
+            password = 'password123',
+            name = 'Test user'
+        )
+    
+    def test_users_listed(self):
+        """ Test that users are listed on user page. """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
